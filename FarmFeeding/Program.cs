96636960f55881a34734e29da572914767c23737
Georgia Brockie <geobroc@chbc.school.nz>
2024-02-14 14:57:22 +1300

    Creating a list to make a user selcet a specific breed

--- FarmFeeding/Program.cs
+++ FarmFeeding/Program.cs
@@ -4,9 +4,57 @@ namespace FarmFeeding
 {
     class Program
     {
-        static void Main(string[] args)
+        // Global variables
+
+
+
+        list<string> species = new list<string>() { "Sheep", "Pig", "Cow", "Chicken" };
+
+
+
+        // Methods and/or function
+
+        // Check to see if an interger enter by a user is within range
+        static int CheckSpecies(string question, int min, int max)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(question);
+
+            while(true)
+            {
+                try
+                {
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInt >= min && userInt <= max)
+                    {
+                        return userInt;
+                    }
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max\n}");
+                }
+                catch
+                {
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max\n}");
+                }
+            }
+        }
+
+        static void FarmFeeding()
+        {
+            // Decide which species is used
+            string species = CheckSpecies("Choose a species:\n" +
+                "1. Sheep\n" +
+                "2. Pig\n" +
+                "3. Cow\n" +
+                "4. Chicken\n" +
+                "5. Quit\n", 1, 5);
+
+            Console.Clear();
+        }
+
+        static void main()
+        {
+
         }
     }
+
 }
